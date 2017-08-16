7c6408b7c54cf5b08d7487bb3f491bf7727c6755
Ed Morley <edmorley@users.noreply.github.com>
2017-08-16 11:12:25 +0100

    Export lists of installed packages and validate on Travis
    The docker-build.sh script now exports the list of all packages
    installed in the final images to the working directory. These package
    lists are checked-in, making any deviations both easy to spot locally
    plus allowing for their validation on Travis.

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -13,14 +13,25 @@ DOCKERFILE_DIR="$STACK"
 
 [[ -d "$DOCKERFILE_DIR" ]] || abort fatal: stack "$STACK" not found
 
+write_package_list() {
+    local image_tag="$1"
+    local output_file="${2}/installed-packages.txt"
+    echo '# List of packages present in the final image. Regenerate using docker-build.sh' > "$output_file"
+    docker run --rm "$image_tag" dpkg-query --show --showformat='${Package}\n' >> "$output_file"
+}
+
 display "Building $STACK main image"
 docker build --pull --tag "$IMAGE_TAG" "$DOCKERFILE_DIR" | indent
+write_package_list "$IMAGE_TAG" "$DOCKERFILE_DIR"
 
 if [[ "$STACK" != "cedar-14" ]]; then
     display "Building $STACK build-time image"
+    BUILD_IMAGE_TAG="${IMAGE_TAG}-build"
+    BUILD_DOCKERFILE_DIR="${DOCKERFILE_DIR}-build"
     # The --pull option is not used to ensure the build image variant is based on the
     # main stack image built above, rather than the one last published to Docker Hub.
-    docker build --tag "${IMAGE_TAG}-build" "${DOCKERFILE_DIR}-build" | indent
+    docker build --tag "$BUILD_IMAGE_TAG" "$BUILD_DOCKERFILE_DIR" | indent
+    write_package_list "$BUILD_IMAGE_TAG" "$BUILD_DOCKERFILE_DIR"
 fi
 
 display "Size breakdown..."
