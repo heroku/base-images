eaf96400445a3582cd312f51a09892894468ff0a
Damien Mathieu <dmathieu@salesforce.com>
2018-07-19 16:29:15 +0200

    deploy only the current stack

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,18 +2,13 @@
 
 set -ex
 
-ACCOUNT=herokutest
-
-./docker-build.sh cedar-14 $ACCOUNT/cedar:14
-./docker-build.sh heroku-16 $ACCOUNT/heroku:16
-./docker-build.sh heroku-18 $ACCOUNT/heroku:18
+./docker-build.sh $STACK $IMAGE_TAG
 
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
-docker tag $ACCOUNT/cedar:14 $ACCOUNT/cedar:latest
-docker push $ACCOUNT/cedar:14
-docker push $ACCOUNT/cedar:latest
-docker push $ACCOUNT/heroku:16
-docker push $ACCOUNT/heroku:16-build
-docker push $ACCOUNT/heroku:18
-docker push $ACCOUNT/heroku:18-build
+docker push $IMAGE_TAG
+
+if [ "$STACK" == "cedar-14" ]; then
+  docker tag $IMAGE_TAG herokutest/cedar:latest
+  docker push herokutest/cedar:latest
+fi
