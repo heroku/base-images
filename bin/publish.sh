b077fc1fd7e5d3dc5a15346c79d061ac790254e8
Damien Mathieu <dmathieu@salesforce.com>
2018-07-31 10:02:18 +0200

    use a single equal sign

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,7 +8,7 @@ docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 docker push $IMAGE_TAG
 
-if [ "$STACK" == "cedar-14" ]; then
+if [ "$STACK" = "cedar-14" ]; then
   docker tag $IMAGE_TAG herokutest/cedar:latest
   docker push herokutest/cedar:latest
 fi
