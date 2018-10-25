271dba2333c9757cc373a432524cf22865d38626
Damien Mathieu <dmathieu@salesforce.com>
2018-10-25 07:51:22 -0500

    push release and latest tags when a new release is created

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,15 +2,26 @@
 
 set -ex
 
-tag="${IMAGE_TAG}.nightly"
+nightlyTag="${IMAGE_TAG}.nightly"
 
-./docker-build.sh $STACK $TAG
+./docker-build.sh $STACK $nightlyTag
 
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
-docker push $TAG
+docker push $nightlyTag
 
-if [ "$STACK" = "cedar-14" ]; then
-  docker tag $TAG herokutest/cedar:latest
-  docker push herokutest/cedar:latest
+if [ -n "$TRAVIS_TAG" ]; then
+  releaseTag="${IMAGE_TAG}.${TRAVIS_TAG}"
+  latestTag="${IMAGE_TAG}"
+
+  docker tag $nightlyTag $releaseTag
+  docker tag $nightlyTag $latestTag
+
+  docker push $releaseTag
+  docker push $latestTag
+
+  if [ "$STACK" = "cedar-14" ]; then
+    docker tag $nightlyTag heroku/cedar:latest
+    docker push heroku/cedar:latest
+  fi
 fi
