782e45ea742b4d4a908649a76dea07060455466a
Damien Mathieu <dmathieu@salesforce.com>
2018-10-25 07:44:49 -0500

    push images as nightly builds

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
-./docker-build.sh $STACK $IMAGE_TAG
+tag="${IMAGE_TAG}.nightly"
+
+./docker-build.sh $STACK $TAG
 
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
-docker push $IMAGE_TAG
+docker push $TAG
 
 if [ "$STACK" = "cedar-14" ]; then
-  docker tag $IMAGE_TAG herokutest/cedar:latest
+  docker tag $TAG herokutest/cedar:latest
   docker push herokutest/cedar:latest
 fi
