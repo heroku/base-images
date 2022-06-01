c9b1861e15f7a5294fe775370183a3533b0c0d3e
Josh W Lewis <josh.lewis@salesforce.com>
2022-06-01 15:13:06 -0500

    Additionally push to private registry

--- bin/publish-to-dockerhub.sh
+++ bin/publish-to-dockerhub.sh
@@ -26,18 +26,34 @@ docker push "${dateBuildTag}"
 
 if [[ -v CIRCLE_TAG ]]; then
   releaseTag="${IMAGE_TAG}.${CIRCLE_TAG}"
+  releaseTagPrivate="${PRIVATE_REGISTRY_HOST}/s/${SERVICE_USERNAME}/${releaseTag}"
+
   releaseBuildTag="${IMAGE_TAG}-build.${CIRCLE_TAG}"
+  releaseBuildTagPrivate="${PRIVATE_REGISTRY_HOST}/s/${SERVICE_USERNAME}/${releaseBuildTag}"
+
   latestTag="${IMAGE_TAG}"
+  latestTagPrivate="${PRIVATE_REGISTRY_HOST}/s/${SERVICE_USERNAME}/${latestTag}"
+
   latestBuildTag="${IMAGE_TAG}-build"
+  latestBuildTagPrivate="${PRIVATE_REGISTRY_HOST}/s/${SERVICE_USERNAME}/${latestBuildTag}"
+
+  PRIVATE_REGISTRY_TOKEN=$(set +x; curl -f -X POST $SERVICE_TOKEN_ENDPOINT -d "{\"username\":\"$SERVICE_USERNAME\",\"password\":\"$SERVICE_PASSWORD\"}" -s --retry 3 | jq -r ".raw_id_token")
+  (set +x; echo "${PRIVATE_REGISTRY_TOKEN}" | docker login $PRIVATE_REGISTRY_HOST -u "$PRIVATE_REGISTRY_USERNAME" --password-stdin)
 
   docker tag "${nightlyTag}" "${releaseTag}"
+  docker tag "${nightlyTag}" "${releaseTagPrivate}"
   docker tag "${nightlyTag}" "${latestTag}"
+  docker tag "${nightlyTag}" "${latestTagPrivate}"
 
   docker push "${releaseTag}"
+  docker push "${releaseTagPrivate}"
   docker push "${latestTag}"
+  docker push "${latestTagPrivate}"
 
   docker tag "${nightlyBuildTag}" "${releaseBuildTag}"
+  docker tag "${nightlyBuildTag}" "${releaseBuildTagPrivate}"
   docker tag "${nightlyBuildTag}" "${latestBuildTag}"
+  docker tag "${nightlyBuildTag}" "${latestBuildTagPrivate}"
 
   docker push "${releaseBuildTag}"
   docker push "${latestBuildTag}"
