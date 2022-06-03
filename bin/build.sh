8f2ecf2cccf87fe467be14491d4febfeb5f995c3
Josh W Lewis <josh.w.lewis@gmail.com>
2022-06-03 09:40:11 -0500

    Publish images to private registry (#215)
    * Additionally push to private registry
    
    * Rename script
    
    * Quote env vars for shellcheck
    
    * Rework image builds and publishing for internal registry naming constraints
    
    * Update documentation for build script
    
    * Add some commentary back into build.sh
    
    * Add quotes for shellcheck
    
    * Build the correct image for heroku:STACK_VERSION-cnb-build
    
    * Update bin/publish-to-registries.sh
    
    Co-authored-by: Ed Morley <501702+edmorley@users.noreply.github.com>
    
    * Update SERVICE_ env var names
    
    Co-authored-by: Ed Morley <501702+edmorley@users.noreply.github.com>

--- bin/build.sh
+++ bin/build.sh
@@ -5,38 +5,45 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 . bin/stack-helpers.sh
 
-[ $# -eq 3 ] || abort "usage: $(basename "${BASH_SOURCE[0]}") STACK IMAGE_NAME BUILD_IMAGE_NAME"
+[ $# -eq 1 ] || abort "usage: $(basename "${BASH_SOURCE[0]}") STACK_VERSION"
 
-STACK=$1
-IMAGE_TAG=$2
-BUILD_IMAGE_TAG=$3
-DOCKERFILE_DIR="$STACK"
-
-echo "${DOCKERFILE_DIR}"
-
-[[ -d "${DOCKERFILE_DIR}" ]] || abort "fatal: stack ${STACK} not found"
+STACK_VERSION=$1
 
 write_package_list() {
     local image_tag="$1"
     local output_file="${2}/installed-packages.txt"
-    if [[ $image_tag =~ "-cnb" ]]; then
-      echo 'Skipping package list for CNB image'
-    else
-      echo '# List of packages present in the final image. Regenerate using bin/build.sh' > "$output_file"
-      docker run --rm "$image_tag" dpkg-query --show --showformat='${Package}\n' >> "$output_file"
-    fi
+    echo '# List of packages present in the final image. Regenerate using bin/build.sh' > "$output_file"
+    docker run --rm "$image_tag" dpkg-query --show --showformat='${Package}\n' >> "$output_file"
 }
 
-display "Building ${STACK} main image"
-docker build --pull --tag "${IMAGE_TAG}" "${DOCKERFILE_DIR}" | indent
-write_package_list "${IMAGE_TAG}" "${DOCKERFILE_DIR}"
+RUN_IMAGE_TAG="heroku/heroku:${STACK_VERSION}"
+RUN_DOCKERFILE_DIR="heroku-${STACK_VERSION}"
+[[ -d "${RUN_DOCKERFILE_DIR}" ]] || abort "fatal: directory ${RUN_DOCKERFILE_DIR} not found"
+display "Building ${RUN_DOCKERFILE_DIR} / ${RUN_IMAGE_TAG} Heroku runtime image"
+docker build --pull --tag "${RUN_IMAGE_TAG}" "${RUN_DOCKERFILE_DIR}" | indent
+write_package_list "${RUN_IMAGE_TAG}" "${RUN_DOCKERFILE_DIR}"
 
-display "Building $STACK build-time image"
-BUILD_DOCKERFILE_DIR="${DOCKERFILE_DIR}-build"
-# The --pull option is not used to ensure the build image variant is based on the
-# main stack image built above, rather than the one last published to Docker Hub.
+# The --pull option is not used for variants to ensure they are based on the
+# runtime stack image built above, rather than the one last published to DockerHub.
+
+BUILD_IMAGE_TAG="${RUN_IMAGE_TAG}-build"
+BUILD_DOCKERFILE_DIR="${RUN_DOCKERFILE_DIR}-build"
+display "Building ${BUILD_DOCKERFILE_DIR} / ${BUILD_IMAGE_TAG} Heroku build-time image"
 docker build --tag "$BUILD_IMAGE_TAG" "$BUILD_DOCKERFILE_DIR" | indent
 write_package_list "$BUILD_IMAGE_TAG" "$BUILD_DOCKERFILE_DIR"
+
+# write_package_list is not needed for *cnb* variants, as they don't install
+# any additional packages over their non-*cnb* counterparts.
+
+CNB_RUN_IMAGE_TAG="${RUN_IMAGE_TAG}-cnb"
+CNB_RUN_DOCKERFILE_DIR="${RUN_DOCKERFILE_DIR}-cnb"
+display "Building ${CNB_RUN_DOCKERFILE_DIR} / ${CNB_RUN_IMAGE_TAG} CNB runtime image"
+docker build --tag "$CNB_RUN_IMAGE_TAG" "$CNB_RUN_DOCKERFILE_DIR" | indent
+
+CNB_BUILD_IMAGE_TAG="${RUN_IMAGE_TAG}-cnb-build"
+CNB_BUILD_DOCKERFILE_DIR="${RUN_DOCKERFILE_DIR}-cnb-build"
+display "Building ${CNB_BUILD_DOCKERFILE_DIR} / ${CNB_BUILD_IMAGE_TAG} CNB build-time image"
+docker build --tag "$CNB_BUILD_IMAGE_TAG" "$CNB_BUILD_DOCKERFILE_DIR" | indent
 
 display "Size breakdown..."
 docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" \
---
+++ bin/publish-to-registries.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+
+bin/build.sh "${STACK_VERSION}"
+
+# Disable tracing temporarily to prevent logging registry tokens.
+(set +x; echo "${DOCKER_HUB_TOKEN}" | docker login -u "${DOCKER_HUB_USERNAME}" --password-stdin)
+(set +x; curl -f -X POST "$ID_SERVICE_TOKEN_ENDPOINT" -d "{\"username\":\"$ID_SERVICE_USERNAME\",\"password\":\"$ID_SERVICE_PASSWORD\"}" -s --retry 3 | jq -r ".raw_id_token" | docker login "$INTERNAL_REGISTRY_HOST" -u "$INTERNAL_REGISTRY_USERNAME" --password-stdin)
+
+push_group() {
+    local targetTagBase="$1"
+    local targetTagSuffix="$2"
+    for variant in "" "-build" "-cnb" "-cnb-build"; do
+      source="${publicTag}${variant}"
+      target="${targetTagBase}${variant}${targetTagSuffix}"
+      docker tag "${source}" "${target}"
+      docker push "${target}"
+    done
+}
+
+date=$(date -u '+%Y-%m-%d-%H.%M.%S')
+publicTag="heroku/heroku:${STACK_VERSION}"
+privateTag="heroku/heroku-private:${STACK_VERSION}"
+internalTag="${INTERNAL_REGISTRY_HOST}/s/${ID_SERVICE_USERNAME}/heroku:${STACK_VERSION}"
+
+# Push nightly tags to dockerhub (e.g. heroku/heroku:22.nightly)
+push_group "${publicTag}" ".nightly"
+
+# Push date tags to private dockerhub (e.g. heroku/heroku-private:22.2022-06-01-17.00.00)
+push_group "${privateTag}" ".${date}"
+
+if [[ -v CIRCLE_TAG ]]; then
+  # Push release tags to dockerhub (e.g. heroku/heroku:22.v99)
+  push_group "${publicTag}" ".${CIRCLE_TAG}"
+
+  # Push release tags to internal registry
+  push_group "${internalTag}" ".${CIRCLE_TAG}"
+
+  # Push latest/no-suffix tags to dockerhub (e.g. heroku/heroku:22)
+  push_group "${publicTag}" ""
+
+  # Push latest/no-suffix tags to internal registry
+  push_group "${internalTag}" ""
+fi
