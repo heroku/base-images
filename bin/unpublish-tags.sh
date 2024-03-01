e582ef7f2e9fc32f9bf6d6dc9b83500f41acc3d2
Josh W Lewis <josh.lewis@salesforce.com>
2024-03-01 10:17:48 -0600

    Construct targetTagName only once, and reindent bin/unpublish-tags.sh

--- bin/unpublish-tags.sh
+++ bin/unpublish-tags.sh
@@ -5,30 +5,31 @@ set -euo pipefail
 dockerhub_token=$(curl -sS -f --retry 3 --retry-connrefused --connect-timeout 5 --max-time 30 -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DOCKER_HUB_USERNAME}\", \"password\": \"${DOCKER_HUB_TOKEN}\"}" https://hub.docker.com/v2/users/login/ | jq --exit-status -r .token)
 
 unpublish_group() {
-    local stackVersion="$1"
-    local targetTagSuffix="$2"
-    local status=0
-    variants=("" "-build")
-    if (( stackVersion <= 22 )); then
-        variants+=("-cnb" "-cnb-build")
-    fi
-    for variant in "${variants[@]}"; do
-      echo "Deleting heroku/heroku:${stackVersion}${variant}${targetTagSuffix}"
-      response=$(curl -sS --retry 3 --retry-connrefused --connect-timeout 5 --max-time 30 -X DELETE \
-            -H "Authorization: JWT ${dockerhub_token}" \
-            "https://hub.docker.com/v2/namespaces/heroku/repositories/heroku/tags/${stackVersion}${variant}${targetTagSuffix}"
-        )
+	local stackVersion="$1"
+	local targetTagSuffix="$2"
+	local status=0
+	variants=("" "-build")
+	if (( stackVersion <= 22 )); then
+		variants+=("-cnb" "-cnb-build")
+	fi
+	for variant in "${variants[@]}"; do
+		targetTagName="${stackVersion}${variant}${targetTagSuffix}"
+		echo "Deleting heroku/heroku:${targetTagName}"
+		response=$(curl -sS --retry 3 --retry-connrefused --connect-timeout 5 --max-time 30 -X DELETE \
+			-H "Authorization: JWT ${dockerhub_token}" \
+			"https://hub.docker.com/v2/namespaces/heroku/repositories/heroku/tags/${targetTagName}"
+		)
 
-      if [[ -z $response ]]; then
-          >&2 echo "Deleted."
-      elif [[ $response =~ "tag not found" ]]; then
-          >&2 echo "Tag does not exist."
-      else
-          >&2 echo "Couldn't delete. Response: ${response}"
-          status=22
-      fi
-    done
-    return $status
+		if [[ -z $response ]]; then
+			>&2 echo "Deleted."
+		elif [[ $response =~ "tag not found" ]]; then
+			>&2 echo "Tag does not exist."
+		else
+			>&2 echo "Couldn't delete. Response: ${response}"
+			status=22
+		fi
+	done
+	return $status
 }
 
 stackVersion="${1:-$STACK_VERSION}"
