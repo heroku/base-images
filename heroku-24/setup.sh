f733e3603915c1450a126c32f6252b7b2c0321af
David Zuelke <dzuelke@salesforce.com>
2024-07-30 15:05:18 +0200

    Add some missing runtime libraries to heroku-24
    - libgirepository-1.0-1 because of libgirepository1.0-dev and libgirepository-1.0-dev
    - libidn12 because of libidn-dev
    - libmemcachedutil2t64 because of libmemcached-dev
    - libwebpdecoder3 because of libwebp-dev
    - libwmf-0.2-7 because of libwmf-dev
    
    GUS-W-16381871

--- heroku-24/setup.sh
+++ heroku-24/setup.sh
@@ -72,9 +72,11 @@ packages=(
   libgnutls-openssl27
   libgnutls30 # Used by the Ruby and PHP runtimes.
   libharfbuzz-icu0 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
+  libidn12 # Because libidn-dev is in the build image
   liblzf1 # Used by the PHP Redis extension.
   libmagickcore-6.q16-7-extra # Used by the PHP Imagick extension (using the `-extra` package for SVG support).
   libmemcached11 # Used by the PHP Memcached extension.
+  libmemcachedutil2t64 # Same -dev headers as libmemcached11
   libmp3lame0 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
   libmysqlclient21
   libncurses6 # Used by the Ruby runtime.
@@ -94,6 +96,8 @@ packages=(
   libvorbisenc2 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
   libvorbisfile3 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
   libvpx9 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
+  libwebpdecoder3 # Same -dev headers as the other libwebp* packages that get pulled in already
+  libwmf-0.2-7 # Because libwmf-dev is in the build image
   libx264-164 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
   libx265-199 # Used by FFmpeg in heroku-buildpack-activestorage-preview.
   libxslt1.1 # Used by the PHP runtime.
@@ -118,6 +122,12 @@ packages=(
   zip
   zstd
 )
+
+if [[ $(dpkg --print-architecture) == "amd64" ]]; then
+  packages+=(
+    libgirepository-1.0-1 # Because libgirepository1.0-dev and libgirepository-1.0-dev are in the build image
+  )
+fi
 
 apt-get install -y --no-install-recommends "${packages[@]}"
 
