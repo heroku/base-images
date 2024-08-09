dfe4cd5d54d523d029c66ec8b2d61f7c2fa317c1
Josh W Lewis <josh.w.lewis@gmail.com>
2024-08-09 09:24:33 -0500

    Fix bash arguments

--- bin/generate-runtime-images.sh
+++ bin/generate-runtime-images.sh
@@ -6,4 +6,4 @@ STACK_VERSION="${1:-"NAN"}"
 [[ $STACK_VERSION =~ ^[0-9]+$ ]] || (abort "fatal: invalid STACK_VERSION")
 
 sudo cp tools/bin/* /usr/local/bin
-sudo -E convert-to-heroku-stack-image "heroku-${STACK_VERSION}"
+sudo -E convert-to-heroku-stack-image "${STACK_VERSION}"
