c11b4dc66e616190ca068d9dfd935e1df5d5ebe0
David Zuelke <dzuelke@salesforce.com>
2026-03-03 15:44:55 +0100

    Use latest stable Docker in GHA workflows (#378)
    We started using on the `--platform` option of `docker image inspect` in #374, which is only available in Docker 28.1.0+: https://github.com/docker/cli/pull/5934
    
    GitHub recently reverted the changes announced in https://github.com/actions/runner-images/issues/13474 as they were causing too many regressions for users, so the runners for now are back to Docker 28.0.4: https://github.com/actions/runner-images/commit/8323333063d769a66f5985874036fbf97733cb93
    
    Instead of relying on whatever Docker version the runners ship with, we now fetch the latest stable using `docker/setup-docker-action`, which will generally reduce our reliance on runner-images-induced regressions like this in the future.
    
    GUS-W-21410117

--- bin/generate-runtime-images.sh
+++ bin/generate-runtime-images.sh
@@ -6,4 +6,5 @@ STACK_VERSION="${1:-"NAN"}"
 [[ $STACK_VERSION =~ ^[0-9]+$ ]] || (abort "fatal: invalid STACK_VERSION")
 
 sudo cp tools/bin/* /usr/local/bin
-sudo -E convert-to-heroku-stack-image "${STACK_VERSION}"
+# On Debian, /etc/sudoers has secure_path set to prevent PATH forwarding
+sudo -E env "PATH=$PATH" convert-to-heroku-stack-image "${STACK_VERSION}"
